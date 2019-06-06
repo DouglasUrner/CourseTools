fb7ab4fe477e3c23a67e65d7a8ed797c5ab28c12
Douglas Urner <dlu@canishe.com>
2019-06-06 11:02:55 -0700

    Command line args

--- assignment.rb
+++ assignment.rb
@@ -1,4 +1,21 @@
 require 'fileutils'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: assignment [options] name..."
+
+  opts.on("-v", "--[no-]verbose", "Run with verbose narration") do |v|
+    options[:verbose] = v
+  end
+
+  opts.on("-b", "--boilerplate", "Specify location of boilerplate files") do |b|
+    options[:boilerplate] = b
+  end
+end.parse!
+
+p options
+p ARGV
 
 assignment_name = "42"
 
