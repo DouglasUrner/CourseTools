c9d6878d3e450550f7283c9118317664cb750ca2
Douglas Urner <dlu@canishe.com>
2019-06-05 23:28:27 -0700

    Initial commit.

---
+++ assignment.rb
@@ -0,0 +1,13 @@
+require 'fileutils'
+
+var assignment_name = "42"
+
+FileUtils.mkdir assignment_name
+
+// Copy in boilerplate files.
+FileUtils.touch "#{assignment_name}/README.md"
+FileUtils.touch "#{assignment_name}/PITCHME.md"
+FileUtils.touch "#{assignment_name}/index.md"
+
+FileUtils.mkdir "#{assignment_name}/assets"
+FileUtils.touch "#{assignment_name}/assets/.keep"
