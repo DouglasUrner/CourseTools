1cc023114f8bec7e028da592c5df76531ad19d3b
Douglas Urner <dlu@canishe.com>
2019-06-06 23:39:35 -0700

    Add option parsing, start proccesing loop.

--- course.rb
+++ course.rb
@@ -1,3 +1,41 @@
 require 'fileutils'
+require 'optparse'
 
-FileUtils.mkdir "new_course"
+# TODO: read default values.
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: course [options] name..."
+
+  opts.on("-a", "--assignments", "Scafforld assignments") do |a|
+    # TODO ensure that assignment(s) are contained in a unit.
+    options[:assignments] = a
+  end
+
+  opts.on("-b", "--boilerplate", "Folder holding boilerplate files") do |b|
+    options[:boilerplate] = b
+  end
+
+  opts.on("-u", "--unit", "Scafforld unit") do |u|
+    # Units can contain assignments.
+    options[:units] = u
+  end
+
+  opts.on("-v", "--[no-]verbose", "Run with verbose narration") do |v|
+    options[:verbose] = v
+  end
+
+end.parse!
+
+p options
+p ARGV
+
+ARGV.each do |c|
+
+  # Course directory
+  FileUtils.mkdir c
+
+  options[:units].each do |u|
+  end
+
+end
